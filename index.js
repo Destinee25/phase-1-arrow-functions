823f80548de081210f0afd7ec57547cd608c96b2
Destinee Robinson <Destineerobinson08@gmail.com>
2021-07-21 01:52:13 -0400

    commit tree e98e3a6306a972a8ef16ad832466346705436d2e

---
+++ index.js
@@ -0,0 +1,22 @@
+let num1 = 2000
+let num2 = 100 
+
+//has a function expression called divide
+
+    const divide = function(){
+
+//divide divides 2000 by 100    
+
+        return num1 / num2;
+    } 
+
+//has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+    
+    const square = x => x * x
+
+//has an arrow function called add
+//add arrow function take two parameters and adds them together
+
+    const add = (a,b) => 3 + 4
+
